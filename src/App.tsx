e6a18ebd706e3ca42c31884a78668efe41337a41
Loppan1 <139924795+Loppan1@users.noreply.github.com>
2024-10-20 20:48:40 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FlipBook from './components/FlipBook/FlipBook';
 import './App.css';
 
 function App() {
   return (
-    <Router basename="/wc-stallions">
-      <Routes>
-        <Route path="/" element={<FlipBook />} />
-      </Routes>
-    </Router>
+    <FlipBook />
   );
 }
 
