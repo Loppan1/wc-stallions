5bd587f0a07f61e6ed128c80399b1a0c991ce0d8
Loppan1 <139924795+Loppan1@users.noreply.github.com>
2024-10-20 20:03:08 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hingskatalog/',
+  base: '/wc-stallions/',
   plugins: [react()],
 })
